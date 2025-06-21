3cb497456ffccaabce8c4b7fc5f4b56f124e94bd
Arthur Rios Ribeiro <arthurriosribeiro@gmail.com>
2025-06-20 21:55:18 -0300

    Apply locale-aware formatting on value input

--- app/src/main/java/com/arthurriosribeiro/lumen/components/LumenTextField.kt
+++ app/src/main/java/com/arthurriosribeiro/lumen/components/LumenTextField.kt
@@ -1,6 +1,8 @@
 package com.arthurriosribeiro.lumen.components
 
 import android.icu.text.NumberFormat
+import androidx.compose.foundation.interaction.FocusInteraction
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -8,21 +10,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
-import java.lang.Double
+import kotlinx.coroutines.flow.collectLatest
 import java.text.DecimalFormatSymbols
+import java.text.ParseException
 import java.util.Locale
-import kotlin.Boolean
-import kotlin.Int
-import kotlin.String
-import kotlin.Unit
 
 @Composable
 fun LumenTextField(
@@ -43,21 +44,38 @@ fun LumenTextField(
     currencyLocale: Locale = Locale.US
 ) {
 
+    val numberFormat = remember(currencyLocale) {
+        NumberFormat.getNumberInstance(currencyLocale).apply {
+            minimumFractionDigits = 2
+            maximumFractionDigits = 2
+            isGroupingUsed = true
+        }
+    }
+
     val decimalSeparator = remember(currencyLocale) {
         DecimalFormatSymbols.getInstance(currencyLocale).decimalSeparator
     }
 
     TextField(
-        modifier = modifier,
+        modifier = modifier.onFocusChanged { focusState ->
+            if (keyboardType.isKeyboardNumber()) {
+                if (focusState.isFocused) {
+                    value.value = value.value
+                        .replace(Regex("[^\\d$decimalSeparator]"), "")
+                } else {
+                    try {
+                        val parsedNumber = numberFormat.parse(value.value)
+                        if (parsedNumber != null) {
+                            value.value = numberFormat.format(parsedNumber)
+                        }
+                    } catch (_: ParseException) {}
+                }
+            }
+        },
         value = valueString ?: value.value,
         onValueChange = {
-            if (keyboardType == KeyboardType.Number || keyboardType == KeyboardType.Decimal) {
-                value.value = when {
-                    it.isEmpty() ->  it
-                    it == "." ->  "0."
-                    it.count { char -> char == decimalSeparator  } <= 1 && it.matches(Regex("^\\d*\\.?\\d*$")) ->  it
-                    else -> value.value
-                }
+            if (keyboardType.isKeyboardNumber()) {
+                value.value = if (it.matches(Regex("^\\d*\\$decimalSeparator?\\d{0,2}$"))) it else value.value
             } else {
                 value.value = it
             }
@@ -83,3 +101,5 @@ fun LumenTextField(
         minLines = minLines
     )
 }
+
+private fun KeyboardType.isKeyboardNumber() = this == KeyboardType.Number || this == KeyboardType.Decimal
